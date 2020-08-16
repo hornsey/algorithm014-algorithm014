9116529ef91b3eae8b2de1676b2d6d32afba8669
hornsey <hornseyhuang@163.com>
2020-08-16 11:20:07 +0800

    week1 Q189

---
+++ Week_01/Q189SolutionV3.java
@@ -0,0 +1,25 @@
+/**
+ * @author huangtao
+ * @date 2020/8/16
+ * @version 3
+ */
+public class Q189Solution {
+	public void rotate(int[] nums, int k) {
+		int n = nums.length;
+		k %= n;
+
+		int count = 0;
+		for (int i = 0; i < k && count < n; i++) {
+			int curr = i;
+			int prev = nums[i];
+			do {
+				int next = (curr + k) % n;
+				int temp = nums[next];
+				nums[next] = prev;
+				prev = temp;
+				curr = next;
+				count++;
+			} while (i != curr);
+		}
+	}
+}

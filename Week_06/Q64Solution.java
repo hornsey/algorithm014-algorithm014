4d34d907b70da1c2ae93141ad160a19f56fee70e
hornsey <hornseyhuang@163.com>
2020-09-21 00:46:32 +0800

    week6 学习笔记

---
+++ Week_06/Q64Solution.java
@@ -0,0 +1,28 @@
+package Week_06;
+
+/**
+ * @author huangtao
+ * @date 2020/9/21
+ */
+public class Q64Solution {
+	public int minPathSum(int[][] grid) {
+		if (grid.length == 0 || grid[0].length == 0) {
+			return 0;
+		}
+		int m = grid.length, n = grid[0].length;
+		int[][] dp = new int[m][n];
+		dp[0][0] = grid[0][0];
+		for (int i = 1; i < m; i++) {
+			dp[i][0] = dp[i-1][0] + grid[i][0];
+		}
+		for (int j = 1; j < n; j++) {
+			dp[0][j] = dp[0][j-1] + grid[0][j];
+		}
+		for (int i = 1; i < m; i++) {
+			for (int j = 1; j < n; j++) {
+				dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j];
+			}
+		}
+		return dp[m-1][n-1];
+	}
+}
